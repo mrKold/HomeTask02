efc795f73dcc5a9665c925ba03a04b17abf88c7c
IvanK <mr.koldamasov@mail.ru>
2022-10-11 14:32:43 +0300

    проверка на кратность двух чисел к друг другу

---
+++ Ex005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите второе число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b*b || b == a*a)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
