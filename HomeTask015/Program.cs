c3202a5996c3f164113dcc62a1a00052d83335ba
IvanK <mr.koldamasov@mail.ru>
2022-10-11 20:07:55 +0300

    Этот день выходной?

---
+++ HomeTask015/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись! Введите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1 || n == 2 || n == 3 || n == 4 || n == 5)
+     Console.WriteLine("нет");
+else
+     Console.WriteLine("да");
