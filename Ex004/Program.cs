8fdb85cef68a109a9071228a68429c34e20a6fbf
IvanK <mr.koldamasov@mail.ru>
2022-10-11 14:27:43 +0300

    проверка числа на кратность одновременно 7 и 23

---
+++ Ex004/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 7 == 0 && a % 23 == 0)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
