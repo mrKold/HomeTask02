77c6d01a562862d713b3b2aa89654d81b4743497
IvanK <mr.koldamasov@mail.ru>
2022-10-11 13:57:25 +0300

    проверка кратности чисел

---
+++ Ex003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите второе число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0)
+    Console.WriteLine("кратно");
+else
+    Console.WriteLine($"не кратно, остаток {a % b}");
