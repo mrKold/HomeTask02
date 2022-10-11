fd7521e47ba2585bc5dd1f92e7921701d3fcabe9
IvanK <mr.koldamasov@mail.ru>
2022-10-11 20:32:51 +0300

    Вывод третьей цифры заданного числа

---
+++ HomeTask013/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 99)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else 
+{
+    Console.WriteLine(n.ToString()[2]);
+}
+
