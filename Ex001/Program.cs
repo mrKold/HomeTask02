5c308ee666f3076d23bb62244ca134696e19dccb
IvanK <mr.koldamasov@mail.ru>
2022-10-11 13:07:27 +0300

    вывод наибольшего числа из рандомного двузначного числа

---
+++ Ex001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int n = new Random().Next(10, 100);
+int n1 = n/10;
+int n2 = n%10;
+Console.WriteLine($"Сгенерированное число: {n}");
+
+if (n1 > n2)
+    Console.WriteLine(n1);
+else if (n1 < n2)
+    Console.WriteLine(n2);
+else 
+    Console.WriteLine("=");
